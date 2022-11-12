c697ab706e146d42af685256805778964c52262e
Adrian <adrianvalverde45@gmail.com>
2022-11-12 17:55:33 -0500

    fetch list of pokemon

--- project.js
+++ project.js
@@ -1,4 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Dom has loaded")
 
+    fetch("http://localhost:3000/Pokemon")
+        .then((data) => data.json())
+        .then((allPokemon) => {
+            console.log("All pokemon have loaded")
+            updateCards(allPokemon)
+            dropDown(allPokemon)
+            createYourOwn(allPokemon)
+        })
 })
